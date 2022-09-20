c11ce91c6bf80414ea288aef849b8439d49e2e58
Harrison Cole Wilkinson <hcwilkinson22@gmail.com>
2022-09-20 13:11:29 -0500

    added padding

--- pages/index.js
+++ pages/index.js
@@ -2,29 +2,29 @@
 
 export default function Home() {
   return (
-	<>
-   <div className="flex justify-between">
-		<h1 className="text-2xl animate-waving-hand opacity-0">H</h1>
-		<h1 className="text-2xl animate-waving-hand animation-delay-100 opacity-0">A</h1>
-		<h1 className="text-2xl animate-waving-hand animation-delay-200 opacity-0">R</h1>
-		<h1 className="text-2xl animate-waving-hand animation-delay-300 opacity-0">R</h1>
-		<h1 className="text-2xl animate-waving-hand animation-delay-400 opacity-0">I</h1>
-		<h1 className="text-2xl animate-waving-hand animation-delay-500 opacity-0">S</h1>
-		<h1 className="text-2xl animate-waving-hand animation-delay-600 opacity-0">O</h1>
-		<h1 className="text-2xl animate-waving-hand animation-delay-700 opacity-0">N</h1>
+	<div className="flex flex-col justify-between  h-screen">
+   <div className="flex justify-between my-24 mx-12">
+		<h1 className="text-6xl animate-waving-hand opacity-0">H</h1>
+		<h1 className="text-6xl animate-waving-hand animation-delay-100 opacity-0">A</h1>
+		<h1 className="text-6xl animate-waving-hand animation-delay-200 opacity-0">R</h1>
+		<h1 className="text-6xl animate-waving-hand animation-delay-300 opacity-0">R</h1>
+		<h1 className="text-6xl animate-waving-hand animation-delay-400 opacity-0">I</h1>
+		<h1 className="text-6xl animate-waving-hand animation-delay-500 opacity-0">S</h1>
+		<h1 className="text-6xl animate-waving-hand animation-delay-600 opacity-0">O</h1>
+		<h1 className="text-6xl animate-waving-hand animation-delay-700 opacity-0">N</h1>
    </div>
 
-<div className="flex justify-between">
-	<h1 className="text-2xl animate-waving-hand animation-delay-700 opacity-0">W</h1>
-	<h1 className="text-2xl animate-waving-hand animation-delay-800 opacity-0">I</h1>
-	<h1 className="text-2xl animate-waving-hand animation-delay-900 opacity-0">L</h1>
-	<h1 className="text-2xl animate-waving-hand animation-delay-1000 opacity-0">K</h1>
-	<h1 className="text-2xl animate-waving-hand animation-delay-1100 opacity-0">I</h1>
-	<h1 className="text-2xl animate-waving-hand animation-delay-1200 opacity-0">N</h1>
-	<h1 className="text-2xl animate-waving-hand animation-delay-1300 opacity-0">S</h1>
-	<h1 className="text-2xl animate-waving-hand animation-delay-1400 opacity-0">O</h1>
-	<h1 className="text-2xl animate-waving-hand animation-delay-1500 opacity-0">N</h1>
+<div className="flex justify-between my-24 mx-12">
+	<h1 className="text-6xl animate-waving-hand animation-delay-700 opacity-0">W</h1>
+	<h1 className="text-6xl animate-waving-hand animation-delay-800 opacity-0">I</h1>
+	<h1 className="text-6xl animate-waving-hand animation-delay-900 opacity-0">L</h1>
+	<h1 className="text-6xl animate-waving-hand animation-delay-1000 opacity-0">K</h1>
+	<h1 className="text-6xl animate-waving-hand animation-delay-1100 opacity-0">I</h1>
+	<h1 className="text-6xl animate-waving-hand animation-delay-1200 opacity-0">N</h1>
+	<h1 className="text-6xl animate-waving-hand animation-delay-1300 opacity-0">S</h1>
+	<h1 className="text-6xl animate-waving-hand animation-delay-1400 opacity-0">O</h1>
+	<h1 className="text-6xl animate-waving-hand animation-delay-1500 opacity-0">N</h1>
 </div>
-</>
+</div>
   )
 }
