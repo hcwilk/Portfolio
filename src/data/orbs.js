7ae172f81d7599aaec4e8a38874592584437d427
Harrison Wilkinson <cole@0xcoast.com>
2024-04-03 15:30:39 -0500

    weird console error

--- src/data/orbs.js
+++ src/data/orbs.js
@@ -4,9 +4,9 @@ const desktopPositions = [
     { x: -20, y: -13, z: -2, URL: 'https://www.linkedin.com/in/harrison-cole-wilkinson-528310191/', image: 'images/linked.png', label: 'LinkedIn' },
     { x: 0, y: -21, z: -2, URL: "/files/Cole_Wilkinson_Resume.pdf", image: 'images/resume.png', label: 'Resume' },
     { x: 20, y: -13, z: -2, URL: "#contact", image: 'images/mail.png', label: 'Contact' },
-    { x: 20, y: 10, z: -2, URL: "#portfolio", image: 'images/robot.png', label: 'Robotics' },
-    { x: 0, y: 18, z: -2, URL: "#portfolio", image: 'images/lock.png', label: 'Cybersecurity' },
-    { x: -20, y: 10, z: -2, URL: "#portfolio", image: 'images/code.png', label: 'Full-Stack Development' }
+    { x: 20, y: 10, z: -2, URL: "#work", image: 'images/robot.png', label: 'Robotics' },
+    { x: 0, y: 18, z: -2, URL: "#work", image: 'images/lock.png', label: 'Cybersecurity' },
+    { x: -20, y: 10, z: -2, URL: "#work", image: 'images/code.png', label: 'Full-Stack Development' }
 ];
 
 
@@ -14,9 +14,9 @@ const mobilePositions = [
     { x: -12, y: -13, z: -2, URL: 'https://www.linkedin.com/in/harrison-cole-wilkinson-528310191/', image: 'images/linked.png', label: 'LinkedIn' },
     { x: 0, y: -15, z: -2, URL: "/files/Cole_Wilkinson_Resume.pdf", image: 'images/resume.png', label: 'Resume' },
     { x: 12, y: -13, z: -2, URL: "#contact", image: 'images/mail.png', label: 'Contact' },
-    { x: 12, y: 12, z: -2, URL: "#portfolio", image: 'images/robot.png', label: 'Robotics' },
-    { x: 0, y: 14, z: -2, URL: "#portfolio", image: 'images/lock.png', label: 'Cybersecurity' },
-    { x: -12, y: 12, z: -2, URL: "#portfolio", image: 'images/code.png', label: 'Software' }
+    { x: 12, y: 12, z: -2, URL: "#work", image: 'images/robot.png', label: 'Robotics' },
+    { x: 0, y: 14, z: -2, URL: "#work", image: 'images/lock.png', label: 'Cybersecurity' },
+    { x: -12, y: 12, z: -2, URL: "#work", image: 'images/code.png', label: 'Software' }
 ]
 
 
